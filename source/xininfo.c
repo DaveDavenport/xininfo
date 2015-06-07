a354f4827deec7f8a94c3ea2292fb4c23b8e5b87
Qball Cow <qball@gmpclient.org>
2015-06-07 13:06:16 +0200

    Update copyright

--- source/xininfo.c
+++ source/xininfo.c
@@ -2,7 +2,7 @@
  * xininfo
  *
  * MIT/X11 License
- * Copyright (c) 2014 Qball  Cow <qball@gmpclient.org>
+ * Copyright (c) 2014-2015 Qball  Cow <qball@gmpclient.org>
  *
  * Permission is hereby granted, free of charge, to any person obtaining
  * a copy of this software and associated documentation files (the
